9d9966d3fd3f2cc41ffff0802d592fca3c8257b5
joelting02-sys <joelting02@gmail.com>
2026-07-10 23:28:51 +0800

    fix: stop duplicate favorite buttons and restore single corner icon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import FavoritesFeature from './favorites/FavoritesFeature.jsx'
-import FavoriteButtonLayout from './favorites/FavoriteButtonLayout.jsx'
 import '@xyflow/react/dist/style.css'
 import './styles.css'
 import './favorites/favorites.css'
@@ -11,6 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <FavoritesFeature />
-    <FavoriteButtonLayout />
   </React.StrictMode>,
 )
