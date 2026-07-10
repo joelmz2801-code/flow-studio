f15ea57fbaf96acea48029a8cc2fe29f0b676a5a
joelting02-sys <joelting02@gmail.com>
2026-07-10 22:49:26 +0800

    revert: restore app before favorites feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FeatureHub from './FeatureHub.jsx'
 import '@xyflow/react/dist/style.css'
 import './styles.css'
-import './feature-hub.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <FeatureHub />
   </React.StrictMode>,
 )
